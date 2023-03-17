b70f0dfa1271a6d7c0808fde204793cdaa43461a
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-03-17 12:15:09 -0400

    Create difficulty.php

---
+++ difficulty.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Difficulty</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="opening.css">
+    </head>
+
+    <body>
+        <div class="background"></div>
+        <div class="container">
+            <div class="opening">
+                <h1>Select Your Difficulty</h1>
+            </div>
+            <div class="button-container">
+                <a href="generate_suspect.php?difficulty=easy" class="button">Easy</a>
+                <a href="generate_suspect.php?difficulty=medium" class="button">Medium</a>
+                <a href="generate_suspect.php?difficulty=hard" class="button">Hard</a>
+            </div>
+        </div>
+    </body>
+</html>
