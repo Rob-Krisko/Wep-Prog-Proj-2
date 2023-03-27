2bd1151a21b0a28f1c3266e70578757a5d48aa30
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-03-27 12:42:09 -0400

    updated with text animation

--- suspect_B.php
+++ suspect_B.php
@@ -18,6 +18,7 @@ if (isset($_POST['reset'])) {
 // Get the suspect's information
 $name = 'Revy';
 $stmt = "Around the time the shot went off I was in the bar finishing my bottle with Uruk and Joop near. He went to go find Slickback and I decided to head to the foyer just in case there was more than just a single shot coming, you know. That's when I came across this poor young girl, everyone was there except for Slickback, Uruk, and Sanji. ";
+$stmt_lines = explode("\n", wordwrap($stmt, 150, "\n"));
 ?>
 
 <!DOCTYPE html>
@@ -32,7 +33,11 @@ $stmt = "Around the time the shot went off I was in the bar finishing my bottle 
     </div>
     <div class="container">
         <div class="textbox">
-            <p><?php echo $stmt; ?></p>
+            <p>
+                <?php foreach ($stmt_lines as $index => $line): ?>
+                    <span class="line" style="animation-delay: <?php echo $index * 2; ?>s;"><?php echo $line; ?></span><br>
+                <?php endforeach; ?>
+            </p>
         </div>
         <div class="buttons">
             <form method="POST" action="process_guess.php">
