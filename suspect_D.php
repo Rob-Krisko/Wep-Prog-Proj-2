4903d9a0cf3914cdcbf77b9af167139ce7864a24
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-03-27 11:53:29 -0400

    Update suspect_D.php

--- suspect_D.php
+++ suspect_D.php
@@ -16,9 +16,7 @@ if (isset($_POST['reset'])) {
 }
 
 // Get the suspect's information
-$name = 'Suspect D';
-$clue1 = 'Has brown hair';
-$clue2 = 'Wears a tie';
+$name = 'Uruk';
 $stmt = "I was sitting at the bar in the center wing next to where we were playing poker, Joop and I decided to keep the game going. My first initial thought was that Slickback was on another episode again, so I sat back to finish my drink and wait it out. However, I heard one of the newcomers yell out another name. At that point I went to check on Slickback and yelled for him to wake up through the door. He tends to have women over and I didnt want to risk seeing that mess. Then went straight to the foyer and took a look around the scene. I noticed a blonde chunk of hair wrapped around the buckle of the satchel. Terrible thing, that young too, no reason for her to die. ";
 ?>
 
@@ -30,18 +28,16 @@ $stmt = "I was sitting at the bar in the center wing next to where we were playi
 </head>
 <body>
     <div class="background-image">
-    <img src="./images/Suspect D.png"> 
+    <img src="Suspect D.png"> 
     </div>
     <div class="container">
         <div class="textbox">
-            <p><?php echo $clue1; ?></p>
-            <p><?php echo $clue2; ?></p>
             <p><?php echo $stmt; ?></p>
         </div>
         <div class="buttons">
             <form method="POST" action="process_guess.php">
-                <input type="hidden" name="suspect" value="Suspect D">
-                <button type="submit" name="guess" value="Suspect D" class="btn">Guess this suspect</button>
+                <input type="hidden" name="suspect" value="Uruk">
+                <button type="submit" name="guess" value="Uruk" class="btn">Guess this suspect</button>
             </form>
             <form method="POST">
                 <button type="submit" name="back" class="btn">Choose another suspect</button>
