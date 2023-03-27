c3a26828e668902e07c51c6d96ecca6dea161142
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-03-27 11:53:50 -0400

    Update suspect_E.php

--- suspect_E.php
+++ suspect_E.php
@@ -16,9 +16,7 @@ if (isset($_POST['reset'])) {
 }
 
 // Get the suspect's information
-$name = 'Suspect E';
-$clue1 = 'Has red hair';
-$clue2 = 'Has a cane';
+$name = 'Sebastian';
 $stmt = "I was in the process of finding Slickback to see if everything was okay, he seemed to be in a real rush to leave immediately after the last round of drinks came out, I was worried he was not pleased with the taste…. Come to think of it, he was acting really weird as soon as the team abruptly arrived. I was on my way from the east entrance to the foyer to get to his room when the gunshot went off. As soon as I heard it I immediately ran to help with whatever was happening in the mansion. Now thinking back… I do think I saw a puff of smoke float  around the stairs leading a trail into the west wing."
 ?>
 
@@ -30,18 +28,16 @@ $stmt = "I was in the process of finding Slickback to see if everything was okay
 </head>
 <body>
     <div class="background-image">
-    <img src="./images/Suspect E.png"> 
+    <img src="Suspect E.png"> 
     </div>
     <div class="container">
         <div class="textbox">
-            <p><?php echo $clue1; ?></p>
-            <p><?php echo $clue2; ?></p>
             <p><?php echo $stmt; ?></p>
         </div>
         <div class="buttons">
             <form method="POST" action="process_guess.php">
-                <input type="hidden" name="suspect" value="Suspect E">
-                <button type="submit" name="guess" value="Suspect E" class="btn">Guess this suspect</button>
+                <input type="hidden" name="suspect" value="Sebastian">
+                <button type="submit" name="guess" value="Sebastian" class="btn">Guess this suspect</button>
             </form>
             <form method="POST">
                 <button type="submit" name="back" class="btn">Choose another suspect</button>
