e48aeb0bc7322f4e26852abb5f354fa91152551f
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-03-27 21:31:30 -0400

    major update for scoreboard functionality

--- success.php
+++ success.php
@@ -1,3 +1,36 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['score_submitted']) || !$_SESSION['score_submitted']) {
+    // Calculate the elapsed time
+    $elapsed_time = time() - $_SESSION['startTime'];
+
+    // Get user and game-related data
+    $username = $_SESSION['username'];
+    $difficulty = $_SESSION['difficulty'];
+    $used_guesses = $_SESSION['incorrectGuesses'] + 1;
+
+    // Save the user's score to the text file
+    $score_entry = $username . ',' . $difficulty . ',' . $used_guesses . ',' . $elapsed_time . PHP_EOL;
+    file_put_contents('scores.txt', $score_entry, FILE_APPEND);
+
+    // Set the score_submitted session variable to true
+    $_SESSION['score_submitted'] = true;
+
+    // Set the elapsed_time and used_guesses session variables
+    $_SESSION['elapsed_time'] = $elapsed_time;
+    $_SESSION['used_guesses'] = $used_guesses;
+} else {
+    // Retrieve the elapsed time and used guesses from the session
+    $elapsed_time = $_SESSION['elapsed_time'];
+    $used_guesses = $_SESSION['used_guesses'];
+}
+
+// Prepare the user's score message
+$score_message = "You solved the game in {$elapsed_time} seconds with {$used_guesses} guesses!";
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,7 +41,8 @@
 <body>
 	<div class="container">
 		<h2>Congratulations, you guessed correctly!</h2>
-		<p class="message">Good work detective! You solved it and now not only is the book safe again with the team and Sutcliff's friend is officially avenged, we got that rat Sanji getting locked up as we speak!</p>
+		<p class="message">You have won the game!</p>
+		<p class="score"><?php echo $score_message; ?></p>
 		<div class="buttons">
 			<form method="POST" action="reset.php">
 				<button type="submit" class="btn">Play Again?</button>
@@ -16,6 +50,7 @@
 			<form method="POST" action="credits.php">
 				<button type="submit" class="btn">Credits</button>
 			</form>
+			<a href="leaderboard.php" class="btn">Leaderboard</a>
 		</div>
 	</div>
 </body>
